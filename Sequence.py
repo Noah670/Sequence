cfaa93df5348087be6de7c0e4b810279afed571c
Noah <noahrules96950@gmail.com>
2020-01-23 04:20:04 +1000

    Add files via upload
    calculate all the numbers multiplied by themselves up to a specified amount
    
    An increasing geometric sequence

---
+++ Sequence.py
@@ -0,0 +1,21 @@
+# multiplacation
+# calculate all the numbers multiplied by themselves up to a specified  amount
+someNumber = 0
+
+while someNumber <= 5:
+    print(someNumber, 'x', someNumber, '=', someNumber * someNumber)
+    someNumber += 1
+    print(' ')
+
+
+#Geometric sequence that increases
+
+doubleBinary = 2
+print('Geometric sequence increasing!', '\n')
+loop = 0
+print(doubleBinary)
+while loop <= 20:
+    doubleBinary = doubleBinary * 2
+    print(doubleBinary)
+
+    loop += 1
